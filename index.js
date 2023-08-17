ba1a836687ff112bdafc8c6dced80caf8cf89ce8
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-17 14:39:09 -0700

    GET endpoint commit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+
+app.get("/api/v1/tours", (req, res) => {
+  res.json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
