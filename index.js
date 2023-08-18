40f6698b7e5eaffc11f1a07d398209ff174e72d8
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-18 10:40:26 -0700

    server.js create

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const tourRouter = require("./routes/tour.routes");
+const morgan = require();
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
