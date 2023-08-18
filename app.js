a787201aff1e03eff6b14e190b8761353a6dbe1f
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-18 10:37:52 -0700

    server.js  and app.js create

--- app.js
+++ app.js
@@ -6,7 +6,4 @@ app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
